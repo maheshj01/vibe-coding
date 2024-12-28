60915b030ccbd5f9d3a8154c813e0adadc029be4
mahesh jamdade <maheshmn121@gmail.com>
2024-12-28 12:00:04 -0500

    update searchbar UI

--- unsplash-api/src/components/SearchBar.tsx
+++ unsplash-api/src/components/SearchBar.tsx
@@ -23,9 +23,9 @@ const SearchBar: React.FC<SearchProps> = ({ onChange, placeholder, value, onSubm
     return (
         <div className="flex justify-center mt-4">
             <div className="relative flex items-center w-full max-w-md">
-                <FaMagnifyingGlass className="absolute left-3 text-sky-400" />
+                <FaMagnifyingGlass className="absolute left-3 text-black-400" />
                 <input
-                    className="w-full pl-10 border-sky-200 border-2 ring-2 rounded-lg px-4 py-2"
+                    className="w-full pl-10 ring-black ring-2 rounded-lg px-4 py-2 focus:outline-none focus:ring-white-400"
                     type="text"
                     placeholder={placeholder}
                     value={value}
