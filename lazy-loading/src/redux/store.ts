81a3270c9d9bb649a15455c65858e93424203d17
mahesh jamdade <maheshmn121@gmail.com>
2025-01-03 09:22:21 -0500

    update

--- lazy-loading/src/redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import imageReducer from "./images/imageSlice";
-
-const store = configureStore({
-    reducer: imageReducer
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export default store;
